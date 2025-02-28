c4e263103bec9f593f7801d7c1408915dbe84fc1
Omr <omar.bamusa.888@gmail.com>
2025-02-28 20:56:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chit_chat
+
+"Chit Chat" is a dynamic Flutter-based chat application designed to facilitate seamless communication between users. With its sleek design and user-friendly interface, 'Chit Chat' allows individuals to connect effortlessly through text-based messages. Whether you're chatting one-on-one or in a group, 'Chit Chat' ensures a smooth and enjoyable messaging experience, making staying in touch with friends, family, and colleagues easier than ever.
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:chit_chat/screens/welcome_screen.dart';
+import 'package:chit_chat/screens/login_screen.dart';
+import 'package:chit_chat/screens/registration_screen.dart';
+import 'package:chit_chat/screens/chat_screen.dart';
+
+void main() => runApp(ChitChat());
+class ChitChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id : (context) => WelcomeScreen(),
+        LoginScreen.id : (context) => LoginScreen(),
+        RegistrationScreen.id : (context) => RegistrationScreen() ,
+        ChatScreen.id : (context) => ChatScreen() ,
+      },
+    );
+  }
+}
