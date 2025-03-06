35689e0f85b3a9ae6ed67b8e2ab8053e24fbf39f
Omr <omar.bamusa.888@gmail.com>
2025-03-06 17:42:10 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:chit_chat/screens/registration_screen.dart';
 import 'package:chit_chat/screens/chat_screen.dart';
 
 void main() => runApp(ChitChat());
+
 class ChitChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           bodyMedium: TextStyle(color: Colors.black54),
